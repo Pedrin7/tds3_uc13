0e8b00b98d6600b6995f5d76f7740fb848b44aba
Pedrin7 <pedropaim987@gmail.com>
2023-03-22 19:30:27 -0300

    cadastro_imoveis

---
+++ aula02/cadastro_imoveis.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Cadastra Imoveis</title>
+</head>
+<body>
+    <h2>CADASTRO DE IMOVEIS</h2>
+    <br>
+    <form>
+        Nome do projeto: <input type="text" name="nome_projeto"><br>
+    </form>
+</body>
+</html>
