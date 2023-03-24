220afad30d1e75132e582ec319bcb563bc55094a
Pedrin7 <pedropaim987@gmail.com>
2023-03-24 17:21:31 -0300

    Novos arquivos

---
+++ cadastro_imoveis/cadastro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Cadastra Imoveis</title>
+</head>
+<body>
+    <h2>CADASTRO DE IMOVEIS</h2>
+    <br>
+    <form>
+        Nome do projeto <input type="text" name="nome_projeto"><br>
+        Endereço <input type="text" name="endereco"><br>
+        Telefone <input type="text" name="telefone"><br>
+        Cidade <input type="text" name="cidade"><br>
+        Estado <input type="select"  name="estao">
+        
+    </form>
+</body>
+</html>
