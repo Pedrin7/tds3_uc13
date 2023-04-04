dc6b253c213f0e5cbc900455ffc49cb649ac94dc
Pedrin7 <pedropaim987@gmail.com>
2023-04-03 21:53:54 -0300

    Database.php

---
+++ exer0304_database/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database{
+
+    function connect($con = new PDO("mysql:host=localhost;port=3307;dbname=ponto_eletronico", "root", "");)
+    {
+        
+    }
+
+    function close(){
+
+    }
+
+}
+
+
+
+?>
